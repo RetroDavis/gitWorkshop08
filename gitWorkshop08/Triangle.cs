e09cd1993076ebe9947b4e27b6680ff2d05e62c2
MandlaRetro <mmasombuka@retrorabbit.co.za>
2019-01-17 10:56:28 +0200

    Added triangle class

---
+++ gitWorkshop08/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gitWorkshop08 {
+    class Triangle{
+        public static void CalculateArea() {
+            Console.WriteLine("Enter breadth and height: ");
+            int breadth = Int32.Parse(Console.ReadLine());
+            int height = Int32.Parse(Console.ReadLine());
+
+            if (breadth > 0 && height > 0) {
+                int area = (breadth / 2) * height;
+                Console.WriteLine(area);
+            }
+
+        }
+
+    }
+}
