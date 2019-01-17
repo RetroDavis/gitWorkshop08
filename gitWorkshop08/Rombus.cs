c66368ba7547af61fc3cdd2645a594b7c70c793d
Johnty <jmaake@retrorabbit.co.za>
2019-01-17 10:50:12 +0200

    edit remote Robus

---
+++ gitWorkshop08/Rombus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gitWorkshop08
+{
+    class Rombus
+    {
+
+
+
+
+        public static void robusArea(int pHight, int qLength)
+        {
+
+            if (pHight > 0 && qLength > 0)
+            {
+                Console.WriteLine((pHight * qLength) / 2);
+            }
+            else
+            {
+                Console.WriteLine("Both the q and p are supposed to be >0");   
+            }
+
+
+        }
+
+
+
+    }
+}
