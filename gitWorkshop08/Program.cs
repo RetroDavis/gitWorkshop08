0a156a155672aa862976b8e6e2fa916f06682343
MrMarcelM <mmyburg@retrorabbit.co.za>
2019-01-17 10:18:33 +0200

    Initial commit of Project (Project Creation)

---
+++ gitWorkshop08/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitWorkshop08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
